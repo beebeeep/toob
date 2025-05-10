8cebb5766dac062579c3da672d009c5a4d13a47a
Danila Migalin <me@miga.me.uk>
2025-05-10 19:08:03 +0300

    working

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,16 +1,42 @@
-use bytes::BytesMut;
+use bytes::{Buf, BytesMut};
 use prost::Message;
-use std::{io::Write, net::TcpStream};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    net::TcpStream,
+    time::{SystemTime, UNIX_EPOCH},
+};
 use toob::pb;
 
 fn main() {
-    let header = pb::Header {
-        request: pb::Request::Consume as i32,
-    };
     let mut stream = TcpStream::connect("localhost:8137").unwrap();
-    let mut buf = BytesMut::new();
-    header.encode_length_delimited(&mut buf).unwrap();
+    io::stdin().lines().for_each(|line| {
+        let line = line.unwrap();
+        let req = pb::ProduceRequest {
+            topic: "test_topic".to_string(),
+            partition: 0,
+            batch_size: 1,
+        };
+        let mut buf = BytesMut::with_capacity(128);
+        req.encode_length_delimited(&mut buf).unwrap();
+        let header = pb::Header {
+            request_id: pb::Request::Produce as i32,
+            request: buf.to_vec(),
+        };
+        let ts = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+        let msg = pb::Message {
+            timestamp: Some(prost_types::Timestamp {
+                seconds: ts.as_secs() as i64,
+                nanos: ts.subsec_nanos() as i32,
+            }),
+            metadata: HashMap::new(),
+            key: None,
+            value: line.encode_to_vec(),
+        };
 
-    stream.write(&buf).unwrap();
-    println!("CHLOS, {header:?}");
+        buf.truncate(0);
+        header.encode_length_delimited(&mut buf).unwrap();
+        msg.encode_length_delimited(&mut buf).unwrap();
+        stream.write(&buf).unwrap();
+    });
 }
--- src/node/server.rs
+++ src/node/server.rs
@@ -1,8 +1,13 @@
 use std::rc::Rc;
 
-use anyhow::{Context, Result};
-use futures_lite::AsyncReadExt;
-use glommio::net::TcpStream;
+use anyhow::{Context, Result, anyhow};
+use bytes::{Buf, BytesMut};
+use futures_lite::{AsyncRead, AsyncReadExt, AsyncWriteExt, StreamExt};
+use glommio::{
+    ByteSliceExt,
+    io::{DmaStreamWriterBuilder, OpenOptions},
+    net::TcpStream,
+};
 use prost::Message;
 
 use crate::pb;
@@ -18,11 +23,82 @@ impl Server {
         }
     }
 
-    pub async fn process_request(&self, stream: &mut TcpStream) -> Result<()> {
-        let mut buf = [0u8; 64];
-        stream.read(&mut buf).await.context("reading header")?;
-        let header = pb::Header::decode_length_delimited(&buf[..]).context("decoding header")?;
-        eprintln!("got header {header:?}");
+    pub async fn process_request(&self, mut stream: &mut TcpStream) -> Result<()> {
+        // incoming request starts with Header protobuf message prepended with varint-encoded length
+        let header = self
+            .read_delimited_message(&mut stream)
+            .await
+            .context("reading header")?;
+        let header = pb::Header::decode(header.as_ref()).context("decoding header")?;
+
+        match header.request_id.try_into() {
+            Ok(pb::Request::Consume) => self
+                .req_consume(header.request.as_ref(), stream)
+                .await
+                .context("handling consume request")?,
+            Ok(pb::Request::Produce) => self
+                .req_produce(header.request.as_ref(), stream)
+                .await
+                .context("handling produce request")?,
+            Ok(pb::Request::Noop) => {}
+            Err(e) => eprintln!("unknown api request {e}"),
+        };
         Ok(())
+    }
+
+    async fn req_consume(&self, _req: impl Buf, _stream: impl AsyncRead) -> Result<()> {
+        todo!();
+    }
+
+    async fn req_produce(&self, req: impl Buf, mut stream: impl AsyncRead + Unpin) -> Result<()> {
+        let req = pb::ProduceRequest::decode_length_delimited(req).context("decoding request")?;
+        eprintln!("got req {req:?}");
+        let f = match OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .buffered_open(format!(
+                "{}/{}/{}.log",
+                self.log_dir, req.topic, req.partition
+            ))
+            .await
+        {
+            Ok(f) => f,
+            Err(e) => return Err(anyhow!("opening partition file: {e}")),
+        };
+
+        for _ in 0..req.batch_size {
+            let msg = self
+                .read_delimited_message(&mut stream)
+                .await
+                .context("reading message")?;
+            if let Err(e) = f.write_at(msg, 0).await {
+                return Err(anyhow!("writing message: {e}"));
+            }
+        }
+        if let Err(e) = f.close().await {
+            return Err(anyhow!("writing message: {e}"));
+        }
+        Ok(())
+    }
+
+    async fn read_delimited_message(&self, mut stream: impl AsyncRead + Unpin) -> Result<Vec<u8>> {
+        // each message is prepended with varint (from 1 to 10 bytes) encoding its length
+        let mut len_buf = BytesMut::from(&[0; 10][..]);
+        stream
+            .read_exact(&mut len_buf)
+            .await
+            .context("reading message length")?;
+        let len =
+            prost::decode_length_delimiter(&mut len_buf).context("decoding message length")?;
+
+        // we already read 10 bytes, but not all of those are related to length,
+        // likely we already read few bytes of message itself, so chain the remainder
+        let mut buf = vec![0; len];
+        AsyncReadExt::chain(len_buf.as_ref(), stream)
+            .read_exact(&mut buf)
+            .await
+            .context("reading header")?;
+        Ok(buf)
     }
 }
