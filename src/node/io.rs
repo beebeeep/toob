ab6bf2adbd1fdc7c87f68b8e8f73408a1e280743
Danila Migalin <me@miga.me.uk>
2025-06-26 08:50:41 +0300

    process IO requests in separate tasks

--- src/node/io.rs
+++ src/node/io.rs
@@ -1,12 +1,19 @@
 use async_channel::Receiver;
 use snafu::ResultExt;
-use std::collections::{BTreeMap, HashMap};
+use std::{
+    cell::RefCell,
+    collections::{BTreeMap, HashMap},
+    rc::Rc,
+};
 
 use crate::{
     IOMessage, IOReply, IORequest, TopicPartition,
     error::{self, Error},
 };
-use glommio::io::{BufferedFile, OpenOptions, ReadResult};
+use glommio::{
+    io::{BufferedFile, OpenOptions, ReadResult},
+    spawn_local,
+};
 
 #[derive(Debug, Clone, Copy, Default)]
 struct MessageMeta {
@@ -14,9 +21,10 @@ struct MessageMeta {
     size: usize,
 }
 
+#[derive(Clone)]
 pub struct Server {
-    partition_files: HashMap<TopicPartition, BufferedFile>,
-    offset_index: HashMap<TopicPartition, BTreeMap<u64, MessageMeta>>,
+    partition_files: Rc<HashMap<TopicPartition, BufferedFile>>,
+    offset_index: Rc<RefCell<HashMap<TopicPartition, BTreeMap<u64, MessageMeta>>>>,
     tx: Receiver<IOMessage>,
 }
 
@@ -73,42 +81,59 @@ impl Server {
         }
 
         Ok(Self {
-            partition_files,
-            offset_index,
+            partition_files: Rc::new(partition_files),
+            offset_index: Rc::new(RefCell::new(offset_index)),
             tx,
         })
     }
 
     pub async fn serve(&mut self) -> Result<(), Error> {
         loop {
-            match self.tx.recv().await.context(error::IORecv)? {
-                (IORequest::Put { tp, msg }, resp) => {
-                    eprintln!("got PUT to {tp}, {} bytes", msg.len());
-                    let r = self.write_message(&tp, msg).await.map(|v| IOReply::Put(v));
-                    if let Err(e) = resp.send(r) {
-                        eprintln!("error sending PUT reply: {e}");
-                    }
+            let req = self.tx.recv().await.context(error::IORecv)?;
+
+            spawn_local(self.clone().handle_request(req)).detach();
+        }
+    }
+
+    async fn handle_request(mut self, req: (IORequest, oneshot::Sender<Result<IOReply, Error>>)) {
+        match req {
+            (IORequest::Put { tp, msg }, resp) => {
+                eprintln!("got PUT to {tp}, {} bytes", msg.len());
+                let r = self.write_message(&tp, msg).await.map(|v| IOReply::Put(v));
+                if let Err(e) = resp.send(r) {
+                    eprintln!("error sending PUT reply: {e}");
                 }
-                (IORequest::Get { tp, offset }, resp) => {
-                    eprintln!("got GET at {tp}@{offset}");
-                    let r = self
-                        .fetch_message(&tp, &offset)
-                        .await
-                        // I really don't like that we have to copy ReadResult into Vec inside hot path,
-                        // but I failed to find better solution on how to pass it to network thread
-                        .map(|v| IOReply::Get(v.to_vec()));
-                    if let Err(e) = resp.send(r) {
-                        eprintln!("error sending GET reply: {e}");
-                    }
+            }
+            (IORequest::Get { tp, offset }, resp) => {
+                eprintln!("got GET at {tp}@{offset}");
+                let r = self
+                    .fetch_message(&tp, &offset)
+                    .await
+                    // I really don't like that we have to copy ReadResult into Vec inside hot path,
+                    // but I failed to find better solution on how to pass it to network thread
+                    .map(|v| IOReply::Get(v.to_vec()));
+                if let Err(e) = resp.send(r) {
+                    eprintln!("error sending GET reply: {e}");
                 }
             }
         }
     }
 
-    async fn write_message(&mut self, tp: &TopicPartition, message: Vec<u8>) -> Result<u64, Error> {
-        let msg_len = message.len();
-        let idx = self
-            .offset_index
+    async fn write_message(&mut self, tp: &TopicPartition, msg: Vec<u8>) -> Result<u64, Error> {
+        let msg_len = msg.len();
+
+        // write message to partition file (it was opened in append mode)
+        let f = self
+            .partition_files
+            .get(tp)
+            .expect("getting partition file");
+        f.write_at(msg, 0).await.context(error::Glommio {
+            e: "writing to partition file",
+        })?;
+
+        // update offset indexes
+        let mut offset_index = self.offset_index.borrow_mut();
+        let idx = offset_index
             .get_mut(tp)
             .ok_or(Error::TopicPartitionNotFound {
                 topic: tp.topic.clone(),
@@ -120,16 +145,6 @@ impl Server {
             None => (None, None),
         };
 
-        // write message to partition file (it is opened in append mode)
-        let f = self
-            .partition_files
-            .get(tp)
-            .expect("getting partition file");
-        f.write_at(message, 0).await.context(error::Glommio {
-            e: "writing to partition file",
-        })?;
-
-        // update offset indexes
         let (message_offset, message_meta) = (
             last_offset.map_or(0, |x| x + 1),
             MessageMeta {
@@ -146,14 +161,15 @@ impl Server {
 
     async fn fetch_message(&self, tp: &TopicPartition, offset: &u64) -> Result<ReadResult, Error> {
         let meta = {
-            self.offset_index
-                .get(tp)
+            let idx = self.offset_index.borrow();
+            idx.get(tp)
                 .ok_or(Error::TopicPartitionNotFound {
                     topic: tp.topic.clone(),
                     partition: tp.partition,
                 })?
                 .get(&offset)
                 .ok_or(Error::OffsetNotFound)? // TODO: if offset is not YET found, mb we can wait here until it'll be written?
+                .clone()
         };
         let f = self
             .partition_files
