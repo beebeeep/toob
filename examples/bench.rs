1e979e2fb9d6eaf267ddba5ae05a926e3a3237e8
Danila Migalin <me@miga.me.uk>
2025-06-29 23:39:20 +0300

    working on benchmark

--- examples/bench.rs
+++ examples/bench.rs
@@ -1,4 +1,7 @@
-use clap::{Arg, Command};
+use std::sync::Arc;
+
+use bytes::BytesMut;
+use clap::{Arg, Command, Id, value_parser};
 use futures_lite::StreamExt;
 use futures_util::pin_mut;
 use glommio::{CpuSet, LocalExecutor, LocalExecutorPoolBuilder};
@@ -13,21 +16,42 @@ fn main() {
                 .short('s')
                 .help("rps per worker")
                 .required(true)
-                .action(clap::ArgAction::Set),
-            Arg::new("workers")
-                .short('w')
-                .help("number of workers")
+                .value_parser(value_parser!(u32)),
+            Arg::new("producers")
+                .short('p')
+                .help("number of producers")
                 .required(true)
-                .action(clap::ArgAction::Set),
+                .value_parser(value_parser!(usize)),
+            Arg::new("consumers")
+                .short('c')
+                .help("number of consumers")
+                .required(true)
+                .value_parser(value_parser!(usize)),
         ])
         .get_matches();
-    let rps = matches.get_one::<u64>("rps").unwrap();
-    let workers = matches.get_one::<usize>("workers").unwrap();
-    let ex = LocalExecutorPoolBuilder::new(glommio::PoolPlacement::MaxSpread(
-        *workers,
+    let rps = matches.get_one::<u32>("rps").unwrap();
+    let producers = matches.get_one::<usize>("producers").unwrap();
+    let consumers = matches.get_one::<usize>("consumers").unwrap();
+
+    let producer_handles = LocalExecutorPoolBuilder::new(glommio::PoolPlacement::MaxSpread(
+        *producers,
         Some(CpuSet::online().unwrap()),
     ))
-    .on_all_shards(|| async { todo!() });
+    .on_all_shards(|| async {
+        println!("producer {}", glommio::executor().id());
+    })
+    .unwrap();
+    producer_handles.join_all();
+
+    let consumer_handles = LocalExecutorPoolBuilder::new(glommio::PoolPlacement::MaxSpread(
+        *consumers,
+        Some(CpuSet::online().unwrap()),
+    ))
+    .on_all_shards(|| async {
+        println!("consumer {}", glommio::executor().id());
+    })
+    .unwrap();
+    consumer_handles.join_all();
 }
 
 #[derive(Clone, PartialEq, Message)]
@@ -36,6 +60,10 @@ struct Probe {
     id: u64,
     #[prost(bytes, tag = "2")]
     payload: Vec<u8>,
+}
+
+async fn dispatcher(in_msg: async_channel::Receiver<Probe>, out_msg: async_channel::Sender<Probe>) {
+    todo!()
 }
 
 async fn producer(in_msg: async_channel::Receiver<Probe>) {
@@ -49,12 +77,16 @@ async fn producer(in_msg: async_channel::Receiver<Probe>) {
     }
 }
 
-async fn consumer(out_msg: async_channel::Sender<Probe>) {
+async fn consumer(out_msg: async_channel::Sender<Probe>, partition: u32) {
     let mut client = client::Client::connect("localhost:8137").await.unwrap();
-    let messages = client.consume("test_topic", 0, 0, None).await.unwrap();
+    let messages = client
+        .consume("test_topic", partition, 0, None)
+        .await
+        .unwrap();
     pin_mut!(messages);
     while let Some(msg) = messages.next().await {
-        let msg = msg.unwrap();
-        println!("{msg:?}")
+        let (_, msg) = msg.unwrap();
+        let msg = Probe::decode(msg.value.as_ref()).unwrap();
+        out_msg.send(msg).await.unwrap();
     }
 }
